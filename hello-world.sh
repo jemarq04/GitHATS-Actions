d66168fe02a5b6db2a1172cdfbfdcc0655a1b535
Justin Marquez <justin.marquez@wisc.edu>
2024-07-15 12:08:30 -0500

    modified echo statements

--- hello-world.sh
+++ hello-world.sh
@@ -3,6 +3,7 @@
 
 echo -e "Running ${0} ...\n================"
 echo -e "Hello world! My name is ${1} ${2}, but most people call me ${3}."
+echo -e "I will now take my leave."
 
 # If the script exits with something other than 0 the GitHub Action will fail.
 exit 0
